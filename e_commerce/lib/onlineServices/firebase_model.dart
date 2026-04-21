51f5e46e75c1f7bab538508faa183318055cac3d
Low Wei Han <weihan.low@softspace.com.my>
2026-04-21 17:09:00 +0800

    + Added Firestore.

--- e_commerce/lib/onlineServices/firebase_model.dart
+++ e_commerce/lib/onlineServices/firebase_model.dart
@@ -1,16 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:e_commerce/constants/strings.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class FirebaseModel {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
-  Future<String> registerFirebaseAccount(String email, String password) async {
+  Future<String> registerFirebaseAccount({
+    required String email,
+    required String password,
+    required String firstName,
+    required String lastName,
+    required String username,
+    required String phoneNumber,
+  }) async {
     try {
       final credential = await _firebaseAuth.createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
-      await credential.user?.sendEmailVerification();
+      final user = credential.user;
+      if (user == null) {
+        return firebaseUnknownError;
+      }
+      try {
+        await FirebaseFirestore.instance.collection('users').doc(user.uid).set({
+          'email': email,
+          'firstName': firstName,
+          'lastName': lastName,
+          'username': username,
+          'phoneNumber': phoneNumber,
+          'createdAt': FieldValue.serverTimestamp(),
+        }, SetOptions(merge: true));
+      } on FirebaseException catch (e) {
+        return e.message ?? firebaseProfileSaveFailed;
+      }
+      await user.sendEmailVerification();
       return firebaseEmailVerificationSent;
     } on FirebaseAuthException catch (e) {
       if (e.code == "weak-password") {
